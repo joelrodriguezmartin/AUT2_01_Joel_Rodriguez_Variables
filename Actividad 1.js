1f27c04bcc190e0cec4461cfd56a396eb51d5b75
joelrodriguezmartin <joloeol@gmail.com>
2021-09-27 09:08:36 +0100

    Actividad 1

--- Actividad 1.js
+++
@@ -1,5 +0,0 @@
-var number = 15;
-for (var i = 0; i <= 6; i++){
-    number = number*number
-};
-alert(number);
---
+++ Actividad1.js
@@ -0,0 +1,8 @@
+
+function actividad1() {
+    var number = 15;
+    for (var i = 0; i <= 6; i++) {
+        number = number * number
+    };
+    alert(number);
+}
